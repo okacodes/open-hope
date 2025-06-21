f971ced1a0a6a22fcf6e47a77bdf69585700344e
Logan Lyons <nospecdev@gmail.com>
2025-06-20 23:20:00 -0500

    Moving away from a modal for the signup and login page, I think I

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,26 @@ registerBtn.addEventListener('click', () => {
 
 }
 )
+
+signupSubmitBtn.addEventListener('submit')
+
+// submit form data using AJAX (avoid page refreshing)
+document.querySelector('.signup-form').addEventListener('submit', async (def) => {
+  def.preventDefault(); // Prevent the default form submission
+
+  // define form fields for submission
+  userName = document.querySelector('.userName').value.trim()
+  email = document.querySelector('.email').value.trim()
+  password = document.querySelector('.password').value.trim()
+  confirmPassword = document.querySelector('.confirmPassword').value.trim()
+
+  const res = await fetch('/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userName, email, password, confirmPassword)
+  })
+
+
+})
